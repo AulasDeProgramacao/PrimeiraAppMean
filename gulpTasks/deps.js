a18d03362a75820924fc1cbb6ae36d4aaf6c8637
Leonardo Leitão <leonardomleitao@gmail.com>
2016-08-18 14:01:37 -0300

    simplificando a estrutura do projeto

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -7,25 +7,25 @@ gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts']);
 
 gulp.task('deps.js', function() {
   gulp.src([
-    'bower_components/angular/angular.min.js',
-    'bower_components/angular-ui-router/release/angular-ui-router.min.js',
-    'bower_components/angular-animate/angular-animate.min.js',
-    'bower_components/angular-toastr/dist/angular-toastr.tpls.min.js',
-    'bower_components/AdminLTE/plugins/jQuery/jquery-2.2.3.min.js',
-    'bower_components/AdminLTE/plugins/jQueryUI/jquery-ui.min.js',
-    'bower_components/AdminLTE/bootstrap/js/bootstrap.min.js',
-    'bower_components/AdminLTE/plugins/morris/morris.min.js',
-    'bower_components/AdminLTE/plugins/sparkline/jquery.sparkline.min.js',
-    'bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
-    'bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
-    'bower_components/AdminLTE/plugins/knob/jquery.knob.js',
-    'bower_components/AdminLTE/plugins/daterangepicker/moment.js',
-    'bower_components/AdminLTE/plugins/daterangepicker/daterangepicker.js',
-    'bower_components/AdminLTE/plugins/datepicker/bootstrap-datepicker.js',
-    'bower_components/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
-    'bower_components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js',
-    'bower_components/AdminLTE/plugins/fastclick/fastclick.js',
-    'bower_components/AdminLTE/dist/js/app.min.js',
+    'node_modules/angular/angular.min.js',
+    'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+    'node_modules/angular-animate/angular-animate.min.js',
+    'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
+    'node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+    'node_modules/admin-lte/plugins/jQueryUI/jquery-ui.min.js',
+    'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
+    'node_modules/admin-lte/plugins/morris/morris.min.js',
+    'node_modules/admin-lte/plugins/sparkline/jquery.sparkline.min.js',
+    'node_modules/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
+    'node_modules/admin-lte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
+    'node_modules/admin-lte/plugins/knob/jquery.knob.js',
+    'node_modules/admin-lte/plugins/daterangepicker/moment.js',
+    'node_modules/admin-lte/plugins/daterangepicker/daterangepicker.js',
+    'node_modules/admin-lte/plugins/datepicker/bootstrap-datepicker.js',
+    'node_modules/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
+    'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
+    'node_modules/admin-lte/plugins/fastclick/fastclick.js',
+    'node_modules/admin-lte/dist/js/app.min.js',
   ])
   .pipe(uglify())
   .pipe(concat('deps.min.js'))
@@ -34,18 +34,18 @@ gulp.task('deps.js', function() {
 
 gulp.task('deps.css', function() {
   gulp.src([
-    'bower_components/angular-toastr/dist/angular-toastr.min.css',
-    'bower_components/font-awesome/css/font-awesome.min.css',
-    'bower_components/Ionicons/css/ionicons.min.css',
-    'bower_components/AdminLTE/bootstrap/css/bootstrap.min.css',
-    'bower_components/AdminLTE/dist/css/AdminLTE.min.css',
-    'bower_components/AdminLTE/dist/css/skins/_all-skins.min.css',
-    'bower_components/AdminLTE/plugins/iCheck/flat/blue.css',
-    'bower_components/AdminLTE/plugins/morris/morris.css',
-    'bower_components/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.css',
-    'bower_components/AdminLTE/plugins/datepicker/datepicker3.css',
-    'bower_components/AdminLTE/plugins/daterangepicker/daterangepicker-bs3.css',
-    'bower_components/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css',
+    'node_modules/angular-toastr/dist/angular-toastr.min.css',
+    'node_modules/font-awesome/css/font-awesome.min.css',
+    'node_modules/ionicons/css/ionicons.min.css',
+    'node_modules/admin-lte/bootstrap/css/bootstrap.min.css',
+    'node_modules/admin-lte/dist/css/AdminLTE.min.css',
+    'node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
+    'node_modules/admin-lte/plugins/iCheck/flat/blue.css',
+    'node_modules/admin-lte/plugins/morris/morris.css',
+    'node_modules/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.css',
+    'node_modules/admin-lte/plugins/datepicker/datepicker3.css',
+    'node_modules/admin-lte/plugins/daterangepicker/daterangepicker-bs3.css',
+    'node_modules/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css',
   ])
   .pipe(uglifycss({ "uglyComments": true }))
   .pipe(concat('deps.min.css'))
@@ -54,9 +54,9 @@ gulp.task('deps.css', function() {
 
 gulp.task('deps.fonts', function() {
   gulp.src([
-    'bower_components/font-awesome/fonts/*.*',
-    'bower_components/Ionicons/fonts/*.*',
-    'bower_components/bootstrap/fonts/*.*'
+    'node_modules/font-awesome/fonts/*.*',
+    'node_modules/ionicons/fonts/*.*',
+    'node_modules/admin-lte/bootstrap/fonts/*.*'
   ])
   .pipe(gulp.dest('public/assets/fonts'));
 });
