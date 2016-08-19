4eaa687675dafc19557c62de360170ddcf1e4019
Leonardo Leitão <leonardomleitao@gmail.com>
2016-08-19 14:01:39 -0300

    resolvendo o problema do footer do template (adminlte)

--- gulpTasks/app.js
+++ gulpTasks/app.js
@@ -9,28 +9,29 @@ gulp.task('app', ['app.html', 'app.css', 'app.js' , 'app.assets']);
 
 gulp.task('app.html', function() {
   gulp.src('frontend/app/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('public'));
 });
 
 gulp.task('app.css', function() {
-   gulp.src('frontend/app/**/*.css')
-   .pipe(uglifycss({ "uglyComments": true }))
-   .pipe(concat('app.min.css'))
-   .pipe(gulp.dest('public/assets/css'));
+   gulp.src('frontend/**/*.css')
+     .pipe(uglifycss({ "uglyComments": true }))
+     .pipe(concat('app.min.css'))
+     .pipe(gulp.dest('public/assets/css'));
 });
 
 gulp.task('app.js', function() {
   gulp.src('frontend/**/*.js')
-    .pipe(babel({
-      presets: ['es2015']
-    }))
+    .pipe(babel({ presets: ['es2015'] }))
     .pipe(uglify())
     .pipe(concat('app.min.js'))
     .pipe(gulp.dest('public/assets/js'));
 });
 
 gulp.task('app.assets', function() {
-  gulp.src('frontend/assets/**/*.*')
-    .pipe(gulp.dest('public/assets'));
+  gulp.src([
+    'frontend/assets/**/*.*',
+    '!frontend/assets/**/*.css',
+    '!frontend/assets/**/*.js'
+  ]).pipe(gulp.dest('public/assets'));
 });
